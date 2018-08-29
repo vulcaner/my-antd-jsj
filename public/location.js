9c48184744991058c3959f5c492e1f3ea15359d8
lixiaocong <435058290@qq.com>
2018-08-29 16:49:59 +0800

    更新location

--- public/location.js
+++ public/location.js
@@ -1,12 +1,12 @@
 (function (win) {
-    const ip = "10.1.1.90";
-    const port = 8080;
-    const domain = `http:// + ${ ip } + : + ${ port }`;
+    const IP = "10.1.1.90";
+    const PORT = 8080;
+    const DOMAIN = `http:// + ${ IP } + : + ${ PORT }`;
     win.domainPath = { staticPath: "" };
     win.apiConfig= {
         start:{
             name: '',
-            address: domain +"/page1/_search?pretty",
+            address: DOMAIN +"/page1/_search?pretty",
             loopTime: 20000
         }
     };
