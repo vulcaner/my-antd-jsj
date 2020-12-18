b0fe01e4b008cc30dce8602d61d79a4cabab613e
李小聪 <xiaocong.li@vonechain.com>
2020-12-18 17:50:41 +0800

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/style.css';
-import Main from './Main';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Main />, document.getElementById('root'));
-registerServiceWorker();
